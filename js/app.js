93c7b2079280bbb9bab82c92c35b204a5c08f164
Maj <59331721+Majus1@users.noreply.github.com>
2021-07-07 19:28:54 +0200

    Added and tested function getRandomPhraseAsArray

--- js/app.js
+++ js/app.js
@@ -19,11 +19,19 @@ startButton.addEventListener("click", ()=> {
 // Funkcije #1 vzme neko random besedo iz arraya slednjo besedo razbije in vrne novi array sestavljen iz črk razbite besede.
 */
 
-// function getRandomPhraseAsArray(arr){
-    
-// } 
+function getRandomPhraseAsArray(phrase) {
+    /*
+    • Slednja funkcija najde random število med o in dolžino našega arraya.
+    • Poišče element znotraj arraya kateri je bil označen z randomNumber indexom.
+    • Slednji element razbije na posamične in jih shrani v novi array.
+    • Returna nam naš novi array polen črk izbrane besede.
+    */
+    let randomNumber = Math.floor(Math.random() * phrase.length);
+    let randomPhraseSelected = phrase[randomNumber];
+    let lettersSplitFromPgrase = randomPhraseSelected.split("");
+    return lettersSplitFromPgrase;
+} 
 
-console.log(Math.floor(Math.random() * phrases.length + 1));¸
 
 /*
 Man ustvaril si dope funkcijo ki returna random zaporedje nekega elmenta v arraju v celih številih
