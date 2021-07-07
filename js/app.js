301cda511b87c9155560bb934e070e94d9bd3bd7
Maj <59331721+Majus1@users.noreply.github.com>
2021-07-07 20:57:14 +0200

    Edited the addOhraseToDisplay function

--- js/app.js
+++ js/app.js
@@ -32,14 +32,23 @@ function getRandomPhraseAsArray(phrase) {
 // Spodnja vrstica shrani retur funkcije getRandomPhraseAsArray() v spodnjo variablo.
 const phraseArray = getRandomPhraseAsArray(phrase);
 
+    /*
+    • Slednja funkcija najde z pomočjo for loopa prešiba čez vse elemnte znotraj arraya
+    • Selecta div, ul in li element v našemu DOM.
+    • Našemu selectanemu li spremeni text bolj natančno ga nadomesti z elementom iz našega arrya
+    • Vsre li elemente appenda našemu <ul> tagu in BOOM pojavijo se na našim DOM
+    */
 function addPhraseToDisplay(phraseArray) {
     for (let i = 0; i < phraseArray.length; i++) {
-        let rezultat = phraseArray[i];
-        console.log(rezultat);
+        const div = document.querySelector("#phrase");
+        const ul = div.firstElementChild;
+        const li = document.createElement("li");
+        li.textContent = phraseArray[i];
+        ul.appendChild(li);
     };
 };
 
 
 /*
-    ...
+    If the character in the array is a letter and not a space, the function should add the class “letter” to the list item.
 */
